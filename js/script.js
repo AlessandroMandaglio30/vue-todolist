8c274a38ef3d81f66b8429012522d7e6a833f4d9
Alessandro Mandaglio <ale.manda.30@gmail.com>
2021-11-29 15:00:35 +0100

    ESERCIZIO COMPLETO

--- js/script.js
+++ js/script.js
@@ -1,9 +1,39 @@
 const app = new Vue({
     el: "#root",
     data: {
-
+        todos: [
+            {
+                name: "Fare l'esercizio dato da Samuele",
+                done: true
+            },
+            {
+                name: "Fare la spesa",
+                done: true
+            },
+            {
+                name: "Cucinare il pranzo",
+                done: false
+            },
+            {
+                name: "Pulire casa",
+                done: false
+            }
+        ],
+        newTodo: ''
     },
     methods: {
+        addTodo: function () {
+            if (this.newTodo != "") {
 
+                this.todos.push({
+                    name: this.newTodo,
+                    done: false
+                });
+                this.newTodo = '';
+            }
+        },
+        removeTodo: function (todoIndex) {
+            this.todos.splice(todoIndex, 1);
+        }
     }
 });
